7a4cce10991568f2159c0f928af0e53e584c2883
金亮 <jinliang@ihbaby.com>
2016-04-14 15:34:13 +0800

    图片资源的初始化

---
+++ db/db.sql
@@ -0,0 +1,14 @@
+
+/**
+创建用户信息表
+ */
+CREATE TABLE  userInfo
+(
+ id  int(8)  NOT NULL auto_increment,
+lastname varchar(255),
+firstname varchar(255),
+address varchar(255),
+ city varchar(255)
+
+
+);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+
+  host:'112.74.83.167',
+  user:'root',
+  password:'',
+  database:'mysql',
+  port:'3306'
+});
+conn.connect();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  conn.query('select  count(*) as countSize from  user   ',function (err,rows,fields){
+
+    if (err) {
+      throw err;
+    };
+    console.log('the solution is:',rows[0].countSize);
+    res.render('indexhuhu', { title: 'Express' });
+
+  })
+
 });
 
 module.exports = router;
